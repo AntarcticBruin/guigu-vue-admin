10bffe54994430207230e8d95819f62257c891a5
朱克冉 <1343246304@qq.com>
2024-08-25 19:32:56 +0800

    商品管理删除功能

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,11 +2,15 @@ export const getTimeOfDay = () => {
     const now = new Date();
     const hours = now.getHours();
 
-    if (hours >= 5 && hours < 12) {
+    if (hours >= 5 && hours < 7) {
         return '早晨';
-    } else if (hours >= 12 && hours < 18) {
+    } else if
+    (hours >= 7 && hours < 12) {
+        return '上午';
+    } else if
+    (hours >= 12 && hours < 18) {
         return '下午';
-    } else if (hours >= 18 && hours < 21) {
+    } else if (hours >= 18 && hours < 23) {
         return '晚上';
     } else {
         return '凌晨';
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -3,11 +3,41 @@
 </script>
 
 <template>
-  <div>
-    <H1>属性管理</H1>
-  </div>
+  <el-card>
+    <el-form :inline="true">
+      <el-form-item label="一级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card style="margin: 10px 0">
+    <el-button type="primary" size="default" icon="Plus">添加属性</el-button>
+  </el-card>
 </template>
 
 <style scoped lang="scss">
-
+.el-form-item {
+  width: 260px;
+}
 </style>
